20c3e19998e00e7b7af5c674b4257e7ea2f1b3e0
Arron Schaar <aschaar@gmail.com>
2012-04-02 14:47:39 -0700

    created script to copy files into addon

---
+++ scripts/update_addon.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+	path = require('path'),
+	wrench = require('wrench'),
+	addonContentDir = '../addon/web-builder-bootstrap/content';
+	
+	
+var copy = [
+	{ to: 'js/', from: '../public/js' },
+	{ to: 'js/shipyard', from: '../node_modules/shipyard' },
+	{ to: 'js/builder-lib', from: '../node_modules/builder-lib/lib/builder-lib' },
+	{ to: 'controls', from: '../resources/controls' },
+	{ to: 'extensions', from: '../resources/extensions' },
+	{ to: 'project_types', from: '../resources/project_types' },
+]
+
+copy.forEach(function(item){
+
+	var source = path.join(__dirname,item.from);
+	var dest = path.join(__dirname, addonContentDir, item.to);
+	console.log("copying:", source, "\n   =>to:",dest);
+	wrench.mkdirSyncRecursive(dest);
+	wrench.copyDirSyncRecursive(source, dest);
+
+});
